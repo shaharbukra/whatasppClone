72f575b8e83155a9f7a15bcd14a3a1ff09aa3ab4
unknown <shaharbukra@gmail.com>
2020-09-20 23:37:00 +0300

    added built project

---
+++ clone/demo/precache-manifest.ff8876c707a3b310ad0bb7e46d803468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a176b9d9289f628a6e765007c958d39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51278bcfe6c0a8073b45",
+    "url": "/static/css/main.539a1531.chunk.css"
+  },
+  {
+    "revision": "8cd9750e49dfbd137326",
+    "url": "/static/js/2.e8244b25.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.e8244b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51278bcfe6c0a8073b45",
+    "url": "/static/js/main.a06ef3ca.chunk.js"
+  },
+  {
+    "revision": "3b290ee7692c5299b9fc",
+    "url": "/static/js/runtime-main.9e961252.js"
+  }
+]);
