8366b922ce7b14d5b101a2376411bab51ccd32dd
unknown <shaharbukra@gmail.com>
2020-09-22 16:21:42 +0300

    Updates

---
+++ precache-manifest.430c941bbcc48cc110244bd5dfdb3e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625614ddd750e9407f5828ee01d6295d",
+    "url": "/whatasppClone/index.html"
+  },
+  {
+    "revision": "ecf2eb7bc4b41e28cd08",
+    "url": "/whatasppClone/static/css/main.14924a01.chunk.css"
+  },
+  {
+    "revision": "8cd9750e49dfbd137326",
+    "url": "/whatasppClone/static/js/2.e8244b25.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/whatasppClone/static/js/2.e8244b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf2eb7bc4b41e28cd08",
+    "url": "/whatasppClone/static/js/main.13017779.chunk.js"
+  },
+  {
+    "revision": "0e9d89982bfdf4d2e868",
+    "url": "/whatasppClone/static/js/runtime-main.8fe5d4e1.js"
+  }
+]);
